aa5c6bc3e4dc46c0f3e7ff976298c6f588baa855
mariejosea <marie1danez@gmail.com>
2020-12-23 15:12:38 -0400

    últimos cambios

---
+++ src/js/component/TodoList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// forma de utilizar los props
+const TodoList = ({ list, deleteTask }) => {
+	return (
+		<React.Fragment>
+			<div className="card">
+				<ul className="list-group list-group-flush">
+					{list.map((task, index) => {
+						return (
+							<li
+								className="list-group-item list-group-item-info lead d-flex justify-content-between"
+								key={index}
+								onClick={e => {
+									deleteTask(index);
+								}}>
+								{task}
+								<div className="btn btn-danger btn-sm">
+									<i className="fas fa-times" />
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div className="card-footer lead text-muted text-center">
+				{list.length < 1 ? (
+					<p>Nothing to do</p>
+				) : list.length >= 1 ? (
+					<p>Pending Task: {list.length}</p>
+				) : null}
+			</div>
+		</React.Fragment>
+	);
+};
+// props del componente TodoInput
+TodoList.propTypes = {
+	list: PropTypes.array,
+	deleteTask: PropTypes.func
+};
+
+export default TodoList;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,70 +1,12 @@
-import React, { useState } from "react";
-
-//incorpora un componente llamado lista
-
-//include images into your bundle
+import React from "react";
+import TodoInput from "./TodoInput.js";
 
 //create your first component
-const Home = () => {
-	const [task, setTask] = useState("");
-	const [list, setList] = useState([]);
-	const deleteTask = indexTask => {
-		setList(prevState =>
-			prevState.filter((todo, index) => index !== indexTask)
-		);
-	};
-
+export function Home() {
 	return (
 		<div className="container-fluid col-10">
 			<h1 className="text-center">Do you have something to do?</h1>
-
-			<input
-				className="lead col-12"
-				value={task}
-				onKeyDown={e => {
-					if (e.keyCode == 13) {
-						let nuevaLista = [];
-						for (let i = 0; i < list.length; i++) {
-							nuevaLista.push(list[i]);
-						}
-						nuevaLista.push(task);
-						setList(nuevaLista);
-						setTask("");
-					}
-				}}
-				onChange={e => {
-					setTask(e.target.value);
-				}}
-				type="text"
-				placeholder="Write it here"
-			/>
-			<div className="card">
-				<ul className="list-group list-group-flush">
-					{list.map((cosas, index) => {
-						return (
-							<li
-								className="list-group-item list-group-item-info lead d-flex justify-content-between"
-								key={index}
-								onClick={e => {
-									deleteTask(index);
-								}}>
-								{cosas}
-								<div className="btn btn-danger btn-sm">
-									<i className="fas fa-times" />
-								</div>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-			<div className="card-footer lead text-muted text-center">
-				{list.length < 1 ? (
-					<p>Nothing to do</p>
-				) : list.length >= 1 ? (
-					<p>Pending Task: {list.length}</p>
-				) : null}
-			</div>
+			<TodoInput />
 		</div>
 	);
-};
-export default Home;
+}
